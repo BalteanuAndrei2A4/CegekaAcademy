296835997813870014e6c48e89e30875eb497c67
Andrei110969 <andreibalteanu2@gmail.com>
2022-03-11 13:46:35 +0200

    Proiect Extra cu Enumerable

---
+++ Tema 01 - Extra/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1Extra
+{
+    public class Set<T> : IEnumerable<T>
+    {
+        
+        private T[] collection = new T[5]; // declar un array de tip T si initializez cu o valoare aleatorie
+
+        private int numberElements = 0; // numarul de elemtente initializat cu 0
+
+        public void AddElement(T element)
+        {
+            if(numberElements == collection.Length)         //daca array-ul este full, ii dublez dimensiunea
+            {
+                Array.Resize(ref collection, collection.Length * 2);
+
+                
+            }
+            collection[numberElements++] = element;     // adaug noul element
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i=0;i<numberElements;i++)
+            {
+                yield return collection[i];         // iterez si fac yield return
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
---
+++ Tema 01 - Extra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1Extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set<int> genericCollection = new Set<int>();   
+            for(int i = 0; i < 100000000; i++)    //puneti un numar mai mic, eu am pus sa adauge pana la 100000000 sa vad daca imi da OutOfMemoryException.
+            {                                     // Later Edit, la numarul 1000000000 am dat de exceptia cu memoria
+                genericCollection.AddElement(i);
+            }
+
+            foreach (int i in genericCollection)
+            { 
+		Console.WriteLine(i); 
+	    }
+
+            Console.ReadLine();
+        }
+    }
+}
