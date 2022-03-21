95f8701d0ed16a4d48df80848786e59bd8c25345
Balteanu Andrei 2A4 <andreibalteanu2@gmail.com>
2022-03-21 15:58:17 +0200

    Update

---
+++ Tema 03 - Databases/Tema3Databases/Controllers/CarOfferController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tema3Databases.Models;
+
+namespace Tema3Databases.Controllers
+{
+    
+    [ApiController]
+    public class CarOfferController : Controller
+    {
+        private static List<CarOffer> avaibleCars = new List<CarOffer>()
+        {
+            new CarOffer()
+            {
+                Id = 1,
+                Make = "Ford",
+                Model =  "Fiesta",
+                AvailableStock = 100,
+                UnitPrice = 3200,
+            }
+            
+        };
+    [HttpGet("/AvailableCars")]
+    public async Task<ActionResult<List<CarOffer>>> Get()
+    {
+        return Ok(avaibleCars);
+    }
+}
+}
---
+++ Tema 03 - Databases/Tema3Databases/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Tema3Databases.Models;
+
+namespace Tema3Databases.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options ) { }
+
+        public DbSet<CarOffer>  CarOffers { get; set; } 
+    }
+}
